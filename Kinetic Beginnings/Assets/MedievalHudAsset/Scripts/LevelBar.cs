5a87b221b98b21e7b33ca686c621bc3d8aa68195
laz89 <laz89@msn.com>
2016-09-14 23:16:00 -0400

    Uploaded everything Unity
    Uploaded the entire folder for the unity project

---
+++ Kinetic Beginnings/Assets/MedievalHudAsset/Scripts/LevelBar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelBar : MonoBehaviour {
+
+	Text text;
+	public Image image;
+	public int Current = 12534;
+	public int ToNextLevel = 28000;
+	// Use this for initialization
+	void Start () 
+	{
+		text = GetComponentInChildren<Text> ();
+	}
+	
+	void Update()
+	{
+		UpdateText ();
+	}
+	
+	
+	
+	public void UpdateText()
+	{
+		text.text = Current.ToString () + " / " + ToNextLevel.ToString();
+	}
+}
